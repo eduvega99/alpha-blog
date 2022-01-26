6c484b1cd358b10299e0e4242123f636d00b1775
“Eduardo <eduardovr1999@gmail.com>
2022-01-26 10:39:40 +0000

    Add new users signup form

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :articles   #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
-  resources :users
+  resources :users, except: [:new]
 end
